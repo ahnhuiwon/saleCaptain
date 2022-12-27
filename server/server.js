08687ad1ff4e7745e14a0f01d65c40d74e3454ae
ahnhuiwon <huiwonan882@gmail.com>
2022-12-27 15:46:30 +0900

    2022-12-27 업로드

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const mainRouter = require('./router/mainRouter');
-const fmRouter = require('./router/fmRouter');
+// const fmRouter = require('./router/fmRouter');
 const ppomRouter = require('./router/ppomRouter');
 
 const server = require('http').createServer(app);
 
 app.use(cors());
-app.use('/fmkorea', fmRouter);
+// app.use('/fmkorea', fmRouter);
 app.use('/ppomppu', ppomRouter);
 
 server.listen(5000, ()=> {
